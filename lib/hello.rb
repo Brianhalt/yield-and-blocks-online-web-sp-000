eaee0828f023ef8e3395388cc9f62ad27bc2edd3
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-10 15:34:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
-def hello_t(array)
-  i = 0
-  while i < array.length
-    i = i + 1
-  end
-end
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     i = i + 1
+#   end
+# end
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
